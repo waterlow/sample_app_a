a7fa401aa7e52cc91e3bf0e7efa9a98d18ef880c
waterlow <waterlow3442@gmail.com>
2019-01-09 16:49:47 +0900

    13章

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,27 @@
 class Micropost < ApplicationRecord
   default_scope { order(created_at: :desc) }
+
+  CONTENT_TYPE_WHITELIST = %w[image/jpeg image/gif image/png]
+
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
+  validate :picture_size
+  validate :picture_content_type
+
+  has_one_attached :picture
+
+  private
+
+  def picture_size
+    if picture.byte_size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
+
+  def picture_content_type
+    unless picture.content_type.in?(CONTENT_TYPE_WHITELIST)
+      errors.add(:picture, "should be in #{CONTENT_TYPE_WHITELIST}")
+    end
+  end
 end
